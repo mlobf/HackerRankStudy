01a8391205d8d1e711b6cc0bd81f093d618deefc
mlobf <marcoslemeborbafilho@gmail.com>
2021-08-26 21:59:12 -0300

    Lesson 3

--- LessonTwo.py
+++ LessonTwo.py
@@ -13,14 +13,18 @@ Input format must be a single line containing a positive interger
 """
 
 
-x = int(input('Put a positive number'))
+x = int(input('Put a positive number '))
 
 
 if x % 2 != 0:
+    print('impar')
     print('Weird')
-elif x in range(6, 20, 1):
+elif x in range(6, 21, 1):
+    print('E par e Esta neste range de 6 a 20')
     print('Weird')
 elif x in range(2, 5, 1):
+    print('E par e Esta neste range de 6 a 20')
     print('not Weird')
 elif x > 20:
+    print('E par e é maior que 20')
     print('not Weird')
